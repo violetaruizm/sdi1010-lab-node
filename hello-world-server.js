a4751da278b94425f9d3fb16e78dad58feec7b3b
Violeta Ruiz <uo258454@uniovi.es>
2019-04-07 19:50:01 +0200

    SDI-NodeJS-10.6- SW Servicio Memoria

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,6 +1,19 @@
-var http = require('http');
-http.createServer(function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var express = require('express');
+var app = express();
+var os = require('os');
+var puerto = 3000;
+app.get('/memoria', function (req, res) {
+    setTimeout(function () { // Espera de 10 segundos
+
+        console.log(os.freemem());
+        var memoriaLibre = os.freemem() / 1000000; //pasar a MB
+        res.status(200);
+        res.json({
+            memoria: memoriaLibre
+        });
+
+    }, 10000);
+});
+app.listen(puerto, function () {
+    console.log("Servidor listo " + puerto);
+});
